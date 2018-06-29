f339b8c06094d943eca41b1b5a68910bf3a32fff
Tommy Wu <tommywu6@geminiopencloud.com>
2018-06-29 17:44:10 +0800

    build image require file

---
+++ Not-in-django-project/0007_alter_username_on_user.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.core.validators
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('auth', '0006_require_contenttypes_0002'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='user',
+            name='username',
+            field=models.CharField(error_messages={'unique': 'A user with that username already exists.'}, max_length=255, validators=[django.core.validators.RegexValidator('^[\\w.@+-]+$', 'Enter a valid username. This value may contain only letters, numbers and @/./+/-/_ characters.', 'invalid')], help_text='Required. 255 characters or fewer. Letters, digits and @/./+/-/_ only.', unique=True, verbose_name='username'),
+        ),
+    ]
---
+++ Not-in-django-project/run_django_crontab.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -e
+
+python /usr/share/sportal/manage.py crontab add
+
+exit 0
